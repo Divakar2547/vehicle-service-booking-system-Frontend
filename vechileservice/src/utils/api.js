d58e1f591cad84c1d59888c59bc8168e6f95e634
Divakar2547 <divakarganesan57@gmail.com>
2025-12-19 11:21:48 +0530

    Update API base URL to hosted backend on Render

--- vechileservice/src/utils/api.js
+++ vechileservice/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://vehicle-service-booking-system-backend-5.onrender.com/api';
 
 // Get auth token from localStorage
 const getAuthToken = () => {
